c2cf1db9b463825baeff442235f7a75ba5701208
Gianluca Di Bella <gianluca.dib99@gmail.com>
2023-09-05 14:07:57 -0400

    feat: component library components (:

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,82 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const STYLES = {
+    small: {
+      "--font-size": 14 / 16 + "rem",
+      "--border-bottom": 1 + "px",
+      "--icon-size": 16 + "px",
+      "--padding": 8 + "px",
+    },
+    large: {
+      "--font-size": 18 / 16 + "rem",
+      "--border-bottom": 2 + "px",
+      "--icon-size": 24 + "px",
+      "--padding": 12 + "px",
+    },
+  };
+
+  return (
+    <Wrapper
+      style={{
+        "--border-bottom": STYLES[size]["--border-bottom"],
+        "--font-size": STYLES[size]["--font-size"],
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Input
+        placeholder={placeholder}
+        style={{
+          "--width": width + "px",
+        }}
+      />
+      <IconWrapper
+        style={{
+          "--padding": STYLES[size]["--padding"],
+        }}
+      >
+        <Icon id={icon} strokeWidth={2} size={STYLES[size]["--icon-size"]} />
+      </IconWrapper>
+     
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: max-content;
+  display: flex;
+  flex-direction: row-reverse;
+  border-bottom: var(--border-bottom) solid ${COLORS.black};
+  font-size: var(--font-size);
+
+  &:focus-within {
+    outline: 2px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+    outline-offset: 8px;
+  }
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  width: var(--width);
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  ::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
+const IconWrapper = styled.div`
+  padding-right: var(--padding);
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
+      <PresentationalBit>
+      <TextWrapper>{displayedValue}</TextWrapper>
+      <Icon id="chevron-down" strokeWidth={2} size={16} />
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const SelectWrapper = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  height: 0;
+  height: 100%;
+  width: 100%;
+  appearance: none;
+`;
+
+const TextWrapper = styled.span`
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.div`
+   position: relative;
+   width: max-content;
+`;
+
+const PresentationalBit = styled.div`
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 10px 12px 12px;
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  width: fit-content;
+  color: ${COLORS.gray700};
+  border: 2px transparent solid;
+ 
+  ${SelectWrapper}:focus + & {
+    outline : 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+  ${SelectWrapper}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
